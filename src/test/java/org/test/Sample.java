7551e2d2c86b3a2d9e7efded0a2d78aeff849c59
mabubucker <abubucker02@gmail.com>
2022-01-28 20:54:29 +0530

    Joy's code updated

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -3,7 +3,10 @@ package org.test;
 public class Sample {
 	
 	public static void main(String[] args) {
-		
+		System.out.println("Test 4");
+		System.out.println("Test 5");
+		System.out.println("Test 6");
+
 	}
 
 }
